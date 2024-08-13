f78d09d0f89dbebcfcb41777b735e3be588461dd
Jordan Falcon <jfalcon@forumcomm.com>
2024-08-13 10:21:30 -0500

    Initial Commit Main script that checks if correct params are sent. getAdmiralScript pings admiral delivery cdn for correct admiral script

---
+++ index.js
@@ -0,0 +1,61 @@
+import 'dotenv/config'
+import axios from 'axios'
+
+const getAdmiralScript = async (publication, environment) => {
+  let pubId = process.env[publication]
+  console.log(pubId)
+  const response = axios({
+    method: 'get',
+    url: `https://delivery.api.getadmiral.com/script/${pubId}/bootstrap?environment=${environment}`,
+  })
+    .then((response) => {
+      return response.data
+    })
+    .catch((error) => {
+      if (error.response) {
+        return {
+          error: {
+            status: error.response.status,
+            data: error.response.data,
+          },
+        }
+      } else {
+        return 'Error', error.message
+      }
+    })
+  return response
+}
+
+export const handler = async (event) => {
+  if (event === undefined) {
+    return {
+      status: 401,
+      message: 'malformed event',
+    }
+  }
+  if (
+    event.queryParams.publication === undefined ||
+    event.queryParams.publication === null ||
+    event.queryParams.publication === ''
+  ) {
+    return {
+      status: 401,
+      message: 'Invalid Publication',
+    }
+  }
+  if (
+    event.queryParams.environment === undefined ||
+    event.queryParams.environment === null ||
+    event.queryParams.environment === ''
+  ) {
+    return {
+      status: 401,
+      message: 'Invalid environment',
+    }
+  }
+  const admiral = await getAdmiralScript(
+    event.queryParams.publication,
+    event.queryParams.environment
+  )
+  return admiral
+}
